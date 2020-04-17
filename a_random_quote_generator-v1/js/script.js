935a59b3cd1c49e2f8f822ea67c3642019a9d87a
Nunny Reyes <nunnyreyes@gmail.com>
2020-04-17 16:51:30 -0400

    adding quotes

---
+++ a_random_quote_generator-v1/js/script.js
@@ -0,0 +1,55 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+
+const quotes = [
+  {
+    quote: "If you know you are on the right track, if you have this inner knowledge, then nobody can turn you off... no matter what they say.",
+    source: "Barbara McClintock",
+    citation: "Nobel Peace Prize Speech",
+    year: 1983
+  },
+{},
+{},
+{},
+{},
+{},
+{},
+{},
+{},
+{},
+];
+
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+function getRandomQuote() {
+  let number;
+  
+
+}
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
