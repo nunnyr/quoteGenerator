160431997572d74c90db480aca393b1e899217a9
Nunny Reyes <nunnyreyes@gmail.com>
2020-04-20 15:15:45 -0400

    Adding more quotes

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -18,12 +18,30 @@ const quotes = [
     citation: "Nobel Peace Prize Speech",
     year: 1983
   },
-{},
-{},
-{},
-{},
-{},
-{},
+{
+  quote: "Science and everyday life cannot and should not be separated.",
+  source: "Rosalind Franklin",
+
+},
+{
+  quote: "I hadn't been aware that there were doors closed to me until I started knocking on them.",
+  source: "Gertrude B. Elion",
+},
+{
+  quote: "Science and everyday life cannot and should not be separated.",
+  source: "Rosalind Franklin",
+},
+{
+  quote: "Don’t let anyone rob you of your imagination, your creativity, or your curiosity. It’s your place in the world; it’s your life. Go on and do all you can with it, and make it the life you want to live.",
+  source: "Mae Jemison"
+},
+{
+  quote: "We must have perseverance and above all confidence in ourselves. We must believe that we are gifted for something and that this thing must be attained.",
+  source: "Marie Curie",
+},
+{
+  
+},
 {},
 {},
 {},
@@ -37,7 +55,7 @@ const quotes = [
 
 function getRandomQuote() {
   let number;
-  
+
 
 }
 
